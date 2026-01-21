82cb06449625c586b78b83ff83ff06510e4c75dd
vojam1 <vojin.milenkovic5@gmail.com>
2026-01-21 19:40:32 +0100

    Hero page init commit

---
+++ src/Hero.jsx
@@ -0,0 +1,13 @@
+import './Hero.css'
+
+function Hero(){
+    return(
+        <>
+            <h1 class="main-title">FON HAKATON 2026</h1>
+            <h2 class="secondary-title">Use your &lt;code&gt; to change the =road=</h2>
+            <button class="btn-sign-up">PRIJAVI SE!</button>
+        </>
+    );
+};
+
+export default Hero
