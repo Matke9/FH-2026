dad2c572643886b9bce3714eb0c377d9fd4892c1
Nikola Milenkovic <nikolabus17@gmail.com>
2026-02-21 18:08:37 +0100

    cuvam trenutni rad pre pull-a

---
+++ src/OTakmicenju.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Otakmicenju.css';
+
+const OTakmicenju = () =>{
+  return(
+    <div className = "stranica-kontejner">
+      <h1 className = "glavni-naslov">O TAKMICENJU</h1>
+
+      <div className = "sadrzaj-kutija">
+      </div>
+    </div>
+  );
+};
+
+export default OTakmicenju
