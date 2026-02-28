ac544f13b9662e20e6131efcb79465e41c1ede0a
Ognjen Filipovic <ognjen@gmail.com>
2026-02-28 16:10:47 +0100

    sad su komponente u jednom nizu i sem njega sam dodao ovo gde treba da stoji slika i prijavi se dugme

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,50 @@
+
+const navItems = [
+  { label: "O TAKMIČANJU", href: "#o-takmicenju" },
+  { label: "AGENDA", href: "#agenda" },
+  { label: "NAGRADE", href: "#nagrade" },
+  { label: "ISKUSTVA", href: "#iskustva" },
+  { label: "FAQ", href: "#faq" },
+  { label: "ORGANIZACIONI TIM", href: "#tim" },
+  { label: "PARTNERI", href: "#partneri" },
+  { label: "PRAVILNIK", href: "#pravilnik" },
+];
+
+
+export default  function NavBar(){
+    return(
+    <header>
+        <div className="">
+            <a href="/" className="flex items-center gap-2">
+            {}
+            <img
+              src="/logo.png"
+              alt="FON hakaton"
+              className="h-9 w-auto"
+            />
+          </a>
+
+
+            
+            <nav className="">
+                {navItems.map((item) => (
+              <a
+                key={item.label}
+                href={item.href}
+                className=""
+                >
+                {item.label}
+                </a>
+                ))}
+
+                <a
+                 href="#prijava"
+              className=""
+                >
+                 PRIJAVI SE!
+                 </a>
+            </nav>
+        </div>
+    </header>
+    )
+}
