4b396cda7426dbb6c649f9953f014159fd9ba7be
dalno <danilo.stojanovic06@gmail.com>
2026-02-28 16:45:37 +0100

    napravljen responsive design

--- src/components/Nagrade/Nagrade.jsx
+++ src/components/Nagrade/Nagrade.jsx
@@ -24,39 +24,60 @@ function PrizeGlowText({ children, size = "text-[64px]" }) {
 
 export default function Nagrade() {
   return (
-    <section className="relative w-full min-h-screen overflow-hidden bg-[#0B1C2D]">
-      {/* Zvezde*/}
+    <section className="relative w-full min-h-screen overflow-hidden">
+      {/* Zvezde */}
       <img
         src={zvezde}
         alt=""
-        className="absolute inset-0 w-full h-full object-cover z-0"
+        className="absolute inset-0 w-full h-full object-cover z-0 bg-[#0B1C2D]"
       />
 
-      <div className="relative w-full min-h-screen pt-[10vh] z-10">
-        {/* Naslov */}
-        <h2 className="text-center mt-[10%] w-full font-['Dune_Rise'] text-[8vw] text-white">
+      {/* Naslov */}
+      <div className="relative w-full h-screen pt-[10vh] z-10">
+        <h2 className="
+              text-center mt-[33vh] w-full font-['Dune_Rise'] text-[clamp(20px,8vw,120px)] text-white
+              md: mt-[20vh]"
+        >
           nagrade
         </h2>
-
-        {/* 700€ */}
-        <div className="absolute left-[23%] -translate-x-1/2 top-[115%] -translate-y-[100%]">
-          <PrizeGlowText size="text-[clamp(20px,5vw,80px)]">700e</PrizeGlowText>
-        </div>
-
-        {/* 1000€ */}
-        <div className="absolute left-[50%] -translate-x-1/2 top-[100%] -translate-y-[100%]">
-          <PrizeGlowText size="text-[clamp(28px,8vw,130px)]">1000e</PrizeGlowText>
-        </div>
-
-        {/* 500€ */}
-        <div className="absolute left-[77%] -translate-x-1/2 top-[125%] -translate-y-[100%]">
-          <PrizeGlowText size="text-[clamp(18px,6vw,70px)]">500e</PrizeGlowText>
-        </div>
       </div>
 
-      {/* Pustinja */}
-      <div className="relative z-5 w-full mt-auto leading-[0]">
+      {/* Pustinja i nagrade */}
+      <div
+        className="
+          absolute bottom-0 w-full z-20
+          md:relative 
+          md:bottom-auto
+        "
+      >
+        {/* Pustinja */}
         <img src={pustinja} alt="" className="w-full h-auto block" />
+
+        {/* Nagrade */}
+        <div className="absolute inset-0 pointer-events-none">
+          
+          {/* 700€ */}
+          <div className="absolute left-[24%] -translate-x-1/2 bottom-[80%]">
+            <PrizeGlowText size="text-[clamp(12px,5vw,110px)]">
+              700e
+            </PrizeGlowText>
+          </div>
+
+          {/* 1000€ */}
+          <div className="absolute left-1/2 -translate-x-1/2 bottom-[100%]">
+            <PrizeGlowText size="text-[clamp(15px,8vw,130px)]">
+              1000e
+            </PrizeGlowText>
+          </div>
+
+          {/* 500€ */}
+          <div className="absolute left-[75%] -translate-x-1/2 bottom-[70%]">
+            <PrizeGlowText size="text-[clamp(10px,6vw,70px)]">
+              500e
+            </PrizeGlowText>
+          </div>
+
+        </div>
       </div>
     </section>
   );
