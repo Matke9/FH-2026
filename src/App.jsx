e142fc9974b1b3746103012323baf53992d261bd
Nikola Milenkovic <nikolabus17@gmail.com>
2026-03-01 12:10:10 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import { stranice } from './features/pravilnik-data';
 import svitak from './assets/svitak.svg';
 import svitakPhone from './assets/svitakPhone.svg';
 import './index.css';
 import './styles/Pravilnik.css';
 
-function App(){
+function App() {
   const [index, setIndex] = useState(0);
 
-  const sledecaStrana = () =>{
-    if(index < stranice.length - 1) setIndex(index+1);
+  const sledecaStrana = () => {
+    if (index < stranice.length - 1) setIndex(index + 1);
   };
 
-  const prethodnaStrana = () =>{
-    if(index>0) setIndex(index-1);
+  const prethodnaStrana = () => {
+    if (index > 0) setIndex(index - 1);
   };
 
-  return(
+  return (
     <div className='pravilnik-page-wrapper'>
       <div className='bg-overlay'></div>
       <h1 className='naslov-pravilnik'>
@@ -31,34 +31,19 @@ function App(){
           <p className='tekst-pravilnik'>{stranice[index].tekst}</p>
         </div>
       </div>
-<<<<<<< HEAD
       <div className='navigacija-pravilnik'>
         <button onClick={prethodnaStrana} className='strelica'>
           {"<<"}
         </button>
         <span className='broj-strane'>
-          {index+1}
+          {index + 1}
         </span>
         <button onClick={sledecaStrana} className='strelica'>
           {">>"}
-=======
-      <h1 className="font-dune">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
->>>>>>> b96e8aec10b4a2ab4cd16ce4c4291141ba1e1c77
         </button>
       </div>
-<<<<<<< HEAD
     </div>
   );
-=======
-      <p className="read-the-docs font-dune">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
->>>>>>> b96e8aec10b4a2ab4cd16ce4c4291141ba1e1c77
 }
 
-export default App
+export default App;
