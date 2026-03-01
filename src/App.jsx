0a371284e239b22e30b58f05973c6a2878428a2f
Nikola Milenkovic <nikolabus17@gmail.com>
2026-03-01 14:02:10 +0100

    pravilnik 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { stranice } from './features/pravilnik-data';
+import { straniceHackathon, straniceGameJam} from './features/pravilnik-data';
 import svitak from './assets/svitak.svg';
 import svitakPhone from './assets/svitakPhone.svg';
 import './index.css';
@@ -7,18 +7,31 @@ import './styles/Pravilnik.css';
 
 function App() {
   const [index, setIndex] = useState(0);
+  const [tema, setTema] = useState('plava');
+
+  const aktivniPodaci = tema === 'plava' ? straniceHackathon: straniceGameJam;
 
   const sledecaStrana = () => {
-    if (index < stranice.length - 1) setIndex(index + 1);
+    if (index < aktivniPodaci.length - 1) setIndex(index + 1);
   };
 
   const prethodnaStrana = () => {
     if (index > 0) setIndex(index - 1);
   };
 
+  const toggleTema = () =>{
+    setTema(tema === 'plava' ? 'crvena': 'plava');
+    setIndex(0);
+  };
+
   return (
-    <div className='pravilnik-page-wrapper'>
+    <div className={`pravilnik-page-wrapper tema-${tema}`}>
       <div className='bg-overlay'></div>
+
+      <button className='theme-toggle' onClick={toggleTema}>
+        {tema === 'plava' ? 'GAME JAM': 'FON HACKATHON'}
+      </button>
+
       <h1 className='naslov-pravilnik'>
         Pravilnik
       </h1>
@@ -27,8 +40,8 @@ function App() {
         <img src={svitakPhone} className='svitak-bg mobile-only' alt="scroll-mobile" />
         <div className='svitak-boja'></div>
         <div className='svitak-content'>
-          <h2 className='podnaslov-pravilnik'>{stranice[index].naslov}</h2>
-          <p className='tekst-pravilnik'>{stranice[index].tekst}</p>
+          <h2 className='podnaslov-pravilnik'>{aktivniPodaci[index].naslov}</h2>
+          <p className='tekst-pravilnik'>{aktivniPodaci[index].tekst}</p>
         </div>
       </div>
       <div className='navigacija-pravilnik'>
