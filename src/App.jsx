ab095bc6ac0ec132f63015a889974449732aa8b1
Ognjen Filipovic <ognjen@gmail.com>
2026-03-02 22:02:56 +0100

    napravio sam filler stranicu da isprobam da li radi sve, stavio sam u app.css scroll behaviour posto je tako najlakse, ne znam jel to moze da ostane

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/NavBar/NavBar'
 import NavBar from './components/NavBar/NavBar'
+import Filler from './components/filler'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +11,7 @@ function App() {
   return (
     <>
       <NavBar/>
+      <Filler/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
