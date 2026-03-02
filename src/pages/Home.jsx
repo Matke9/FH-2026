c9550dae735d3a68c18c2f3da8677c675e7460c5
Matke9 <matovicmihailo@gmail.com>
2026-03-02 20:30:38 +0100

    merge error resolution

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 // Home page – entry point for the FON Hackathon 2026 website
 import Nagrade from '../components/Nagrade/Nagrade'
+import Agenda from "../components/agenda/Agenda.jsx";
+import Partneri from "../components/Partneri/Partneri.tsx";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Nagrade type="Hackathon"/>
+      <Agenda/>
+      <Partneri/>
     </main>
   )
 }
