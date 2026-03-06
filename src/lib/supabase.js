2c528adea1499603df760401456c94a8ed758720
Matke9 <matovicmihailo@gmail.com>
2026-03-06 18:21:11 +0100

    merge error resolution

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
+
+
 // Zameni sa svojim Supabase credentials
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/tailwind.css'
 import './index.css'
-import Test from './pages/Test.jsx'
+import App from "./App.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Test />
+    <App />
   </StrictMode>,
 )
